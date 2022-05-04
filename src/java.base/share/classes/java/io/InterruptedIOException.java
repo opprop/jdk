e4ec5367aff4ef4fb7c46e8b8d2d6474c82ffb0a
Chris Povirk <cpovirk@google.com>
2022-05-04 16:12:35 -0400

    Annotate two more exception classes for nullness (#15)

--- src/java.base/share/classes/java/io/InterruptedIOException.java
+++ src/java.base/share/classes/java/io/InterruptedIOException.java
@@ -25,6 +25,9 @@
 
 package java.io;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.framework.qual.AnnotatedFor;
+
 /**
  * Signals that an I/O operation has been interrupted. An
  * <code>InterruptedIOException</code> is thrown to indicate that an
@@ -39,6 +42,7 @@ package java.io;
  * @see     java.lang.Thread#interrupt()
  * @since   1.0
  */
+@AnnotatedFor({"nullness"})
 public
 class InterruptedIOException extends IOException {
     private static final long serialVersionUID = 4020568460727500567L;
@@ -60,7 +64,7 @@ class InterruptedIOException extends IOException {
      *
      * @param   s   the detail message.
      */
-    public InterruptedIOException(String s) {
+    public InterruptedIOException(@Nullable String s) {
         super(s);
     }
 
