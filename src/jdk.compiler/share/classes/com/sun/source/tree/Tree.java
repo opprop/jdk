b8e4a58d806dcea92511f01496c050a356eff55a
Haifeng Shi <shihaifeng1998@gmail.com>
2022-08-31 21:56:15 -0400

    Add pure annotation to Tree.getKind() (#21)

--- src/jdk.compiler/share/classes/com/sun/source/tree/Tree.java
+++ src/jdk.compiler/share/classes/com/sun/source/tree/Tree.java
@@ -25,6 +25,8 @@
 
 package com.sun.source.tree;
 
+import org.checkerframework.dataflow.qual.Pure;
+
 /**
  * Common interface for all nodes in an abstract syntax tree.
  *
@@ -665,6 +667,7 @@ public interface Tree {
      *
      * @return the kind of this tree.
      */
+    @Pure
     Kind getKind();
 
     /**
